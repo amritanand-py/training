edcee559fb64ed57c0007dbe45b6cd299d3ce863
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-01-18 13:15:53 +0530

    [Amrit Anand] Add: UC init

---
+++ .netday2/UC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Employess");
+            Console.WriteLine("to wage Computation");
+            Console.ReadLine();
+        }
+    }
+}
