86bedfb8423356832cd113bb09bc46b1f9fee489
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-01-18 13:56:44 +0530

    [Amrit Anand] Add: UC2 code init

---
+++ netday2/UC2 code.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc2_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wageRate = 20;
+            int dayHrs = 8;
+            Console.WriteLine("Your daily wage is : " + (Convert.ToInt32(wageRate*dayHrs)));
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
