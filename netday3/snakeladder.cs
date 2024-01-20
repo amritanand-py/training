c6d7001ea8ff8d05ecdcf40ca97bbb3da28d5db1
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-01-20 13:24:45 +0530

    [Amrit Anand] Update: 2 player setup done

--- netday3/snakeladder.cs
+++ netday3/snakeladder.cs
@@ -19,9 +19,10 @@ namespace snakeLadder
             Console.WriteLine();
             Console.WriteLine("The Player \r\nstarts from 0 rolls the die to get a number between 1 to 6, finds \r\na safe place, ladder or a snake keeps doing till the winning spot \r\n100 is achieved.\r\n");
             int initalPosition = 0;
-            int rolling = letsroll();
-            int currentPosition = 0;
-            Console.WriteLine($"{name}, Your Current Position is :{currentPosition}");
+            //int rolling = letsroll();
+            int currentPosition1 = 0;
+            int currentPosition2 = 0;
+            Console.WriteLine($"{name}, Your Current Position is :{currentPosition1}");
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Let's Begin the game : ");
@@ -34,12 +35,18 @@ namespace snakeLadder
             //ROlling Starts here
             if (input == "")
             {
-                
+
                 int loopcounter = 0;
-                while (currentPosition != 100)
+                while (currentPosition1 != 100)
                 {
 
-                    letsroll();
+                    Console.WriteLine("Player 1 turn: ");
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+                    
+                    int rolling1 = rand.Next(1, 7);
+                    Console.WriteLine("Your Rolling outcome is : " + rolling1);
 
 
                     // UC-3
@@ -49,29 +56,29 @@ namespace snakeLadder
                     switch (decision)
                     {
                         case 1:
-                            currentPosition += 0;
+                            currentPosition1 += 0;
                             Console.WriteLine("Ah no you just hit nil");
-                            Console.WriteLine($"{name}, now Current Position is :{currentPosition}");
+                            Console.WriteLine($"{name}, now Current Position is :{currentPosition1}");
 
                             break;
                         case 2:
-                   
-                           if ((currentPosition+rolling) <= 100)
+
+                            if ((currentPosition1 + rolling1) <= 100)
                             {
-                                currentPosition += rolling;
+                                currentPosition1 += rolling1;
                             }
-                           
+
                             Console.WriteLine("You just ascend ladder");
-                            Console.WriteLine($"{name}, now Current Position is :{currentPosition}");
+                            Console.WriteLine($"{name}, now Current Position is :{currentPosition1}");
                             break;
                         case 3:
-                            currentPosition -= rolling;
+                            currentPosition1 -= rolling1;
                             Console.WriteLine("Oops snake bite you!");
-                            if (currentPosition < 0)
+                            if (currentPosition1 < 0)
                             {
-                                currentPosition = 0;
+                                currentPosition1 = 0;
                             }
-                            Console.WriteLine($"{name}, now Current Position is :{currentPosition}");
+                            Console.WriteLine($"{name}, now Current Position is :{currentPosition1}");
                             break;
                     }
 
@@ -82,23 +89,91 @@ namespace snakeLadder
                     Console.WriteLine();
                     Console.WriteLine();
 
+                    // 2nd player setup
+
+
+                    Console.WriteLine("Player 2 turn: ");
+                    Console.WriteLine();
+                    Console.WriteLine();
+
                     
+                    int rolling2 = rand.Next(1, 7);
+                    Console.WriteLine("Your Rolling outcome is : " + rolling2);
+
+
+                    // UC-3
+
+
+                    int decision2 = rand.Next(1, 4);
+                    switch (decision2)
+                    {
+                        case 1:
+                            currentPosition2 += 0;
+                            Console.WriteLine("Ah no you just hit nil");
+                            Console.WriteLine($"{name}, now Current Position is :{currentPosition2}");
+
+                            break;
+                        case 2:
+
+                            if ((currentPosition2 + rolling2) <= 100)
+                            {
+                                currentPosition2 += rolling2;
+                            }
+
+                            Console.WriteLine("You just ascend ladder");
+                            Console.WriteLine($"{name}, now Current Position is :{currentPosition2}");
+                            break;
+                        case 3:
+                            currentPosition2 -= rolling2;
+                            Console.WriteLine("Oops snake bite you!");
+                            if (currentPosition2 < 0)
+                            {
+                                currentPosition2 = 0;
+                            }
+                            Console.WriteLine($"{name}, now Current Position is :{currentPosition2}");
+                            break;
+                    }
+
+                    // UC-4
+                    // while loop inserted
+                  
+                    
+                    if (currentPosition1 == 100)
+                    {
+                        Console.WriteLine("Player 1 won");
+                    }
+                    else if(currentPosition2 == 100)
+                    {
+                        Console.WriteLine("Player 2 won");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Game will go to another turn");
+                    }
+
+                    Console.WriteLine($"you rolled die {loopcounter} times");
+                    Console.WriteLine();
+                    Console.WriteLine();
+
 
 
                 }
             }
-           
-        }
+            }
 
-
-        static int letsroll()
+        /*static int letsroll()
         {
             Random rand = new Random();
             int rolling = rand.Next(1, 7);
             Console.WriteLine("Your Rolling outcome is : " + rolling);
             return rolling;
-        }
+        }*/
+
+    }
+
+
+        
 
 
     }
-}
+
