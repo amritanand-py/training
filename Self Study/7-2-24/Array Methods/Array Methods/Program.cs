7b911f37e481582493ace426c3acb9a824b1b8ce
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-02-07 12:52:50 +0530

    [Amrit Anand] Add: Application of array methods

---
+++ Self Study/7-2-24/Array Methods/Array Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration of array
+            String[] strdata = { "abc","bcd" };
+            int[] intdata = { 1,2,3,4,5,6,7 };
+            int[] unsortedarray = { 56, 78, 4, 66, 10, 2, 6 };
+
+
+            // Function for printing any kind array  
+            void PrintArray<T>(T[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]+" ");
+                }
+                Console.WriteLine();
+               
+            }
+
+            
+            // Printing length
+            int len = intdata.Length;
+
+            Console.WriteLine("array is  :");
+            PrintArray(intdata);
+            Console.WriteLine("and the length is " + len);
+
+
+
+
+
+
+        }
+    }
+}
